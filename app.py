806907af745be51efe4a36dfa85cac3eea27f76d
zxoou-media <foridulhouk@gmail.com>
2025-09-01 20:38:05 +0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, jsonify, send_from_directory
+from flask_cors import CORS
+import json, os
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+CORS(app)
+
+DATA_FILE = os.path.join(os.path.dirname(__file__), 'data', 'movies.json')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/details/<int:movie_id>')
+def movie_details(movie_id):
+    return render_template('details.html')
+
+@app.route('/api/movies')
+def movies_api():
+    with open(DATA_FILE, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return jsonify(data)
+
+# যদি আপনি ভবিষ্যতে নিজের সার্ভারে ছবি হোস্ট করতে চান
+@app.route('/static/img/<path:filename>')
+def serve_img(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static', 'img'), filename)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
