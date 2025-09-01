66627e9fd40d3ebe5a06fb561695cebdb6ea3c3d
zxoou-media <foridulhouk@gmail.com>
2025-09-01 16:25:23 +0600

    Update app.py

--- cinelistbd/app.py
+++ cinelistbd/app.py
@@ -1,1 +1,11 @@
-ppppp
+import http.server
+import socketserver
+
+PORT = 8000
+
+# SimpleHTTPRequestHandler ব্যবহার করে একটি সাধারণ সার্ভার তৈরি করা
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("সার্ভার চালু হয়েছে http://localhost:8000")
+    httpd.serve_forever()
