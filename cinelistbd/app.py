bd1dc9b71119b74554cf153fd425aab2f6cad4f5
zxoou-media <foridulhouk@gmail.com>
2025-09-02 17:44:35 +0600

    Update app.py

--- cinelistbd/app.py
+++ cinelistbd/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, send_from_directory
 from flask_cors import CORS
-import json, os
+import os, json
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 CORS(app)
@@ -12,19 +12,18 @@ def home():
     return render_template('index.html')
 
 @app.route('/details/<int:movie_id>')
-def movie_details(movie_id):
-    return render_template('details.html')
+def details(movie_id):
+    return render_template('details.html', movie_id=movie_id)
 
 @app.route('/api/movies')
-def movies_api():
+def api_movies():
     with open(DATA_FILE, 'r', encoding='utf-8') as f:
         data = json.load(f)
     return jsonify(data["movies"])
 
 @app.route('/static/img/<path:filename>')
-def serve_img(filename):
+def serve_image(filename):
     return send_from_directory(os.path.join(app.root_path, 'static', 'img'), filename)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True)
